822fcb4e8e1ec61e520a6b2243beb311b34c19cb
pro-grammer-SD <geniussantu1983@gmail.com>
2025-06-21 11:04:59 +0530

    working on updater

--- others/tests/t3.py
+++
@@ -1,3 +0,0 @@
-import urllib.request
-
-urllib.request.urlretrieve("https://miktex.org/download/ctan/systems/win32/miktex/setup/windows-x64/basic-miktex-24.1-x64.exe")
---
+++ update/website/deploy/deploy.bat
@@ -0,0 +1,2 @@
+@echo off
+netlify deploy --dir=. --prod
