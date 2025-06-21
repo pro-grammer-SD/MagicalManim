7ac7aab0a8a6a6af285a9c68806895b55b74aa1a
pro-grammer-SD <geniussantu1983@gmail.com>
2025-06-21 11:22:13 +0530

    up

--- script.py
+++ script.py
@@ -2,6 +2,6 @@ from manim import *
 
 class Output(Scene):
     def construct(self):
-        circle = Circle(radius=3.0, color='#ff04ff')
+        circle = Circle(radius=2, color='#ff9c04')
         self.play(Create(mobject=circle, lag_ratio=1.0, introducer='True'))
         self.interactive_embed()
