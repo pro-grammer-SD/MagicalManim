2208ef2e698cde17c881aed6595fba8134c1480a
pro-grammer-SD <geniussantu1983@gmail.com>
2025-08-24 20:16:59 +0530

    up

--- script.py
+++
@@ -1,5 +0,0 @@
-from manim import *
-
-class Output(Scene):
-    def construct(self):
-        circle = Circle(color='#00ff7f', radius=1.3)
--- temp_scene.py
+++
@@ -1,6 +0,0 @@
-from manim import *
-
-class Output(Scene):
-    def construct(self):
-        circle = Circle(radius=1.3, color='#00ff7f')
-        self.play(Create(mobject=circle, lag_ratio=1.0, introducer=True))
